be273a9e902485563ac182f5ac3f72bfb56b8ae1
vitaliy <viprogramm@gmail.com>
2017-05-16 20:16:45 +0300

    init app

---
+++ src/index.js
@@ -0,0 +1,74 @@
+// @flow
+
+import 'babel-polyfill';
+
+import path from 'path';
+import Koa from 'koa';
+import Pug from 'koa-pug';
+import Router from 'koa-router';
+import koaLogger from 'koa-logger';
+import serve from 'koa-static';
+import middleware from 'koa-webpack';
+import bodyParser from 'koa-bodyparser';
+import session from 'koa-generic-session';
+import flash from 'koa-flash-simple';
+import _ from 'lodash';
+import methodOverride from 'koa-methodoverride';
+import Rollbar from 'rollbar';
+
+import getWebpackConfig from '../webpack.config.babel';
+import addRoutes from './controllers';
+import container from './container';
+
+export default () => {
+  const app = new Koa();
+  const rollbar = new Rollbar('POST_SERVER_ITEM_ACCESS_TOKEN');
+
+  app.keys = ['some secret hurr'];
+  app.use(session(app));
+  app.use(flash());
+  app.use(async (ctx, next) => {
+    ctx.state = {
+      flash: ctx.flash,
+      isSignedIn: () => ctx.session.userId !== undefined,
+    };
+    await next();
+  });
+  app.use(bodyParser());
+  app.use(methodOverride((req) => {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      return req.body._method;
+    }
+  }));
+  app.use(serve(path.join(__dirname, '..', 'public')));
+
+  if (process.env.NODE_ENV !== 'test') {
+    app.use(middleware({
+      config: getWebpackConfig(),
+    }));
+  }
+
+  app.use(koaLogger());
+
+  const router = new Router();
+  addRoutes(router, container);
+  app.use(router.allowedMethods());
+  app.use(router.routes());
+
+  app.use(rollbar.errorHandler());
+
+  const pug = new Pug({
+    viewPath: path.join(__dirname, 'views'),
+    debug: true,
+    pretty: true,
+    compileDebug: true,
+    locals: [],
+    basedir: path.join(__dirname, 'views'),
+    helperPath: [
+      { _ },
+      { urlFor: (...args) => router.url(...args) },
+    ],
+  });
+  pug.use(app);
+  return app;
+};
