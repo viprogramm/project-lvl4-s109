91a481518a30d95875e0a3b044d1f5058ce6a136
vitaliy <viprogramm@gmail.com>
2017-06-15 02:34:30 +0300

    fix init bug

--- src/init.js
+++ src/init.js
@@ -4,7 +4,7 @@ import getModels from './models';
 export default async () => {
   const models = getModels(connect);
   await Promise.all(Object.values(models).map(model => model.sync()));
-  models.TaskStatus.bulkCreate([
+  await models.TaskStatus.bulkCreate([
     { name: 'new' },
     { name: 'in progress' },
     { name: 'testing' },
